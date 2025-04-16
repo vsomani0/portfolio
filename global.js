7e4f26f95174e981859928a9f1e037c28d75dba7
vsomani0 <vsomani0@gmail.com>
2025-04-16 10:42:35 -0700

    Update global.js

--- global.js
+++ global.js
@@ -1,16 +1,5 @@
 console.log('IT’S ALIVE!');
 
-function $$(selector, context = document) {
-  return Array.from(context.querySelectorAll(selector));
-}
-let navLinks = $$("nav a")
-let currentLink = navLinks.find((a) => a.host === location.host && a.pathname === location.pathname)
-currentLink?.classList.add('current');
-const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1") 
-? "/" 
-: "website"
-
-
 let pages = [
   {url: '', title: 'Home'},
   {url: '../contact/', title: 'Contact'},
@@ -27,5 +16,18 @@ nav.append(ul)
 for (let p of pages) {
   let url = p.url
   let title = p.title
-  ul.insertAdjacentHTML('beforeend', `<a href ="${url}">"${title}"</a>`)
+  let li = document.createElement('li')
+  li.insertAdjacentHTML('beforeend', `<a href ="${url}">"${title}"</a>`)
 }
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+let navLinks = $$("nav a")
+let currentLink = navLinks.find((a) => a.host === location.host && a.pathname === location.pathname)
+currentLink?.classList.add('current');
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1") 
+? "/" 
+: "website"
+
+
