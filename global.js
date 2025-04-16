e05298919c4f6256a2d282339c44a4f1fc97d77b
vsomani0 <vsomani0@gmail.com>
2025-04-16 10:40:46 -0700

    Update progress bar

--- global.js
+++ global.js
@@ -6,6 +6,10 @@ function $$(selector, context = document) {
 let navLinks = $$("nav a")
 let currentLink = navLinks.find((a) => a.host === location.host && a.pathname === location.pathname)
 currentLink?.classList.add('current');
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1") 
+? "/" 
+: "website"
+
 
 let pages = [
   {url: '', title: 'Home'},
@@ -13,17 +17,15 @@ let pages = [
   {url: '../projects/', title: 'Projects'},
   {url: "https://github.com/vsomani0", },
   {url: '../resume/', title: 'Resume'},
-
-
 ]
 let nav = document.createElement('nav');
 document.body.prepend(nav);
-// let ul = document.createElement('ul');
-// nav.append(ul)
+let ul = document.createElement('ul');
+nav.append(ul)
 
 
 for (let p of pages) {
   let url = p.url
   let title = p.title
-  nav.insertAdjacentHTML('beforeend', `<a href ="${url}">"${title}"</a>`)
+  ul.insertAdjacentHTML('beforeend', `<a href ="${url}">"${title}"</a>`)
 }
