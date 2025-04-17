131ee058e9214d9a61394e8e36a6bce4fda8526d
vsomani0 <vsomani0@gmail.com>
2025-04-17 12:14:29 -0700

    Fixed urls

--- global.js
+++ global.js
@@ -4,7 +4,7 @@ let pages = [
   {url: '', title: 'Home'},
   {url: '../contact/', title: 'Contact'},
   {url: '../projects/', title: 'Projects'},
-  {url: "https://github.com/vsomani0", },
+  {url: "https://github.com/vsomani0", title: 'Github'},
   {url: '../resume/', title: 'Resume'},
 ]
 let nav = document.createElement('nav');
@@ -13,8 +13,19 @@ let ul = document.createElement('ul');
 nav.appendChild(ul)
 
 
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1") 
+? "/" 
+: "website"
+
+function convert_url(url) {
+  // Allow for usage on website
+  if (!url.startsWith('http')) {
+    return BASE_PATH + url
+  }
+};
+
 for (let p of pages) {
-  let url = p.url
+  let url = convert_url(p.url)
   let title = p.title
   let li = document.createElement('li')
   li.insertAdjacentHTML('beforeend', `<a href ="${url}">${title}</a>`)
@@ -23,12 +34,10 @@ for (let p of pages) {
 
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
-}
+};
 let navLinks = $$("nav a")
 let currentLink = navLinks.find((a) => a.host === location.host && a.pathname === location.pathname)
 currentLink?.classList.add('current');
-const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1") 
-? "/" 
-: "website"
+
 
 
