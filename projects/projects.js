d2e09cb3cb28d974a887cd8a8b7173a8ea70fc45
vsomani0 <vsomani0@gmail.com>
2025-04-25 21:53:49 -0700

    Fixed bug

--- projects/projects.js
+++ projects/projects.js
@@ -6,7 +6,7 @@ const projectsContainer = document.querySelector('.projects');
 console.log(projectsContainer)
 renderProjects(projects, projectsContainer, 'h2');
 
-const projectCount = $$('.projects > article').length;
+const projectCount = document.querySelectorAll('.projects > article').length;
 const h1Tag = document.querySelector('body > h1')
 h1Tag.innerText = projectCount + ' ' + h1Tag.innerText
 
