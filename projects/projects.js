39fb591edb14b5f9fba24d0ca6c6bb302ad67315
vsomani0 <vsomani0@gmail.com>
2025-04-25 20:56:07 -0700

    Added count of projects

--- projects/projects.js
+++ projects/projects.js
@@ -6,5 +6,8 @@ const projectsContainer = document.querySelector('.projects');
 console.log(projectsContainer)
 renderProjects(projects, projectsContainer, 'h2');
 
+const projectCount = $$('.projects > article').length;
+const h1Tag = document.querySelector('body > h1')
+h1Tag.innerText = projectCount + ' ' + h1Tag.innerText
 
 
